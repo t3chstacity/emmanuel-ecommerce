9797e66cbb70c33863383bf11ecf271a803c95b2
t3chstacity <125920888+t3chstacity@users.noreply.github.com>
2024-02-02 04:58:16 -0800

    backlinking

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,10 +38,7 @@ const IndexPage = () => {
 
       {/* Message Container */}
       <div className={styles.messageContainer}>
-        <p>
-          This is a demonstration of the Sydney theme for verse by{' '}
-          <span className={styles.gold}>matter design.</span>
-        </p>
+
         <p>
           wear by <span className={styles.gold}>sunspel</span> and{' '}
           <span className={styles.gold}>scotch&soda</span>
